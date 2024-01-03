9461e630a6b2dc672f0d45e9f1d82133d325eef2
whistlermoth <andreanmuhammad99@gmail.com>
2024-01-03 11:29:24 +0800

    hari kerja

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -150,8 +150,7 @@
                               <div class="col-4 text-center mb-3">
                                 <p class="text mb-1">Sisa Hari Kerja</p>
                                 @if($record)
-                                <h6>0</h6>
-                                {{-- <h6 class="mb-1"> {{ $record->first()->sisa_minggu_kerja_sd_des_23 * 5 }}</h6> --}}
+                                <h6 class="mb-1"> {{ $record->first()->sisa_minggu_kerja_sd_des_23 * 5 }}</h6>
                                 {{-- <!--<h6 class="mb-1">{{ $daysLeft }} Hari</h6>--> --}}
                                 @endif
                               </div>
@@ -169,8 +168,7 @@
                               <div class="col-4 text-center mb-3">
                                 <p class="text mb-1">Sisa Minggu Kerja</p>
                                 @if(!empty($record))
-                                <h6>0</h6>
-                                {{-- <h6 class="mb-1">{{ $record->first()->sisa_minggu_kerja_sd_des_23 }} Minggu</h6> --}}
+                                <h6 class="mb-1">{{ $record->first()->sisa_minggu_kerja_sd_des_23 }} Minggu</h6>
                                 @endif
                               </div>
                             </div>
