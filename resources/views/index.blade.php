f815334fe6dcc0d72362b255361c339bb0c86c78
whistlermoth <andreanmuhammad99@gmail.com>
2024-02-26 18:02:23 +0800

    ganti tgl

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -169,7 +169,7 @@
                                     <b>{{ $request->region_all }}</b><br>
                                 @endif
                                 @if(!empty($record))
-                                {{ \Carbon\Carbon::parse('19-02-2024')->translatedFormat('d F Y') }}
+                                {{ \Carbon\Carbon::parse('26-02-2024')->translatedFormat('d F Y') }}
                                 @endif
                               </div>
                               @php
