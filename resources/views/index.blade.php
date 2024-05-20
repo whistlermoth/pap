aa0251c6f41ba9545889239ab992bea075cc4af5
whistlermoth <andreanmuhammad99@gmail.com>
2024-05-20 13:10:13 +0800

    Naik tgl

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -169,7 +169,7 @@
                                     <b>{{ $request->region_all }}</b><br>
                                 @endif
                                 @if(!empty($record))
-                                {{ \Carbon\Carbon::parse('13-05-2024')->translatedFormat('d F Y') }}
+                                {{ \Carbon\Carbon::parse('20-05-2024')->translatedFormat('d F Y') }}
                                 @endif
                               </div>
                               @php
