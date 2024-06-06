7c916fe155b9abf6873d0a11bf5f7664105846f6
whistlermoth <andreanmuhammad99@gmail.com>
2024-06-06 19:00:00 +0800

    naik tgl

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -169,7 +169,7 @@
                                     <b>{{ $request->region_all }}</b><br>
                                 @endif
                                 @if(!empty($record))
-                                {{ \Carbon\Carbon::parse('27-05-2024')->translatedFormat('d F Y') }}
+                                {{ \Carbon\Carbon::parse('31-05-2024')->translatedFormat('d F Y') }}
                                 @endif
                               </div>
                               @php
